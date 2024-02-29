f95d99979fde90e98055e9c247cc1d4fea04db28
Youngwoo Cho <youngwch@c6r7s5.42seoul.kr>
2024-02-29 16:53:13 +0900

    d

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -15,6 +15,8 @@ mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_P
 mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin shutdown
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'"
+
+mysqladmin -p"'${MYSQL_ROOT_PASSWORD}'" shutdown
 
 exec "$@"
--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -2,15 +2,21 @@
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
 
-cp -r /wordpress/* /var/www/html/ \
-&& cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php \
-&& sed -i "s/database_name_here/${MYSQL_DATABASE}/g" /var/www/html/wp-config.php \
-&& sed -i "s/username_here/${MYSQL_USER}/g" /var/www/html/wp-config.php \
-&& sed -i "s/password_here/${MYSQL_PASSWORD}/g" /var/www/html/wp-config.php \
-&& sed -i "s/localhost/mariadb/g" /var/www/html/wp-config.php \
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+cd /var/www/html
+wp core download --allow-root
+
+wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --dbprefix=wp_ --allow-root
+wp core install --url=${INCEPTION_URL} --title=${INCEPTION_TITLE} --admin_user=${ADMIN_USER} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL} --allow-root
+wp user create ${USER_NAME} ${USER_EMAIL} --role=subscriber --user_pass=${USER_PASSWORD} --allow-root
 
 fi
 
+
 mkdir -p /run/php
+
 
 exec "$@"

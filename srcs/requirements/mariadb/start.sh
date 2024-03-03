8a1ef32d527c32753a80954a59fc5ce4a0cf21e5
Youngwoo Cho <youngwch@c6r7s5.42seoul.kr>
2024-03-03 11:10:08 +0900

    d

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -1,6 +1,12 @@
 # #!/bin/bash
 
+while true; do
+sleep 1;
+done;
+
+
 sed -i '/^bind-address/s/=.*/= 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+# sed -i 's/character-set-server\s*=\s*utf8mb4/character-set-server = utf8mb4_bin/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 mariadb-install-db --datadir=/var/lib/mysql
 
@@ -16,7 +22,10 @@ mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'"
+mysql -p"'${MYSQL_ROOT_PASSWORD}'" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'"
+mysql -p"'${MYSQL_ROOT_PASSWORD}'" -e "FLUSH PRIVILEGES;"
 
+# mysqladmin -p"'${MYSQL_ROOT_PASSWORD}'" shutdown
 mysqladmin -p"'${MYSQL_ROOT_PASSWORD}'" shutdown
 
 exec "$@"
